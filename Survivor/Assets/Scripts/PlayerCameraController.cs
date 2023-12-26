dd23346f1a4b599aec3bae792124fd3a98db4919
LukasSchuster <l.schuster1993@googlemail.com>
2023-12-26 15:25:36 +0100

    Fixed: Camera Controller

--- Survivor/Assets/Scripts/PlayerCameraController.cs
+++ Survivor/Assets/Scripts/PlayerCameraController.cs
@@ -1,39 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviour
 {
-    public PlayerMovementController playerMovementController;
-    public float Sensivity = 500f;
-    public float ClampAngle = 85f;
+    public Transform Orientation;
+    public Transform Player;
+    public Transform PlayerObject;
 
-    private float verticalRotation;
-    private float horizontalRotation;
+    public float RotationSpeed;
 
     private void Start()
     {
-        verticalRotation = transform.localEulerAngles.x;
-        horizontalRotation = transform.eulerAngles.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        Look();
-        Debug.DrawLine(transform.position, transform.forward * 2, Color.red);
-    }
+        Vector3 viewDirection = Player.position - new Vector3(transform.position.x, Player.position.y, transform.position.z);
+        Orientation.forward = viewDirection.normalized;
 
-    private void Look()
-    {
-        float mouseVertical = -Input.GetAxis("Mouse Y");
-        float mouseHorizontal = Input.GetAxis("Mouse X");
-
-        verticalRotation += mouseVertical * Sensivity * Time.deltaTime;
-        horizontalRotation += mouseHorizontal * Sensivity * Time.deltaTime;
-
-        verticalRotation = Mathf.Clamp(verticalRotation, -ClampAngle, ClampAngle);
-
-        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
-        playerMovementController.transform.rotation = Quaternion.Euler(0f, horizontalRotation, 0f);
+        if (viewDirection != Vector3.zero)
+        {
+            PlayerObject.forward = Vector3.Slerp(PlayerObject.forward, viewDirection.normalized, RotationSpeed * Time.deltaTime);
+        }
     }
 }
