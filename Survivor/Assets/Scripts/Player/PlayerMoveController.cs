3eec04d717aed2548b76c557e00006b06f87a8fb
LukasSchuster <l.schuster1993@googlemail.com>
2024-01-07 21:29:57 +0100

    Updated: Jumping

--- Survivor/Assets/Scripts/Player/PlayerMoveController.cs
+++ Survivor/Assets/Scripts/Player/PlayerMoveController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerMoveController : MonoBehaviour
 {
-    [Header("Movement Speed")]
+    [Header("Movement Values")]
     [Range(1f, 2f)]
     public float SprintMultiplier = 1.3f;
+    [Range(500f, 3000f)]
+    public float JumpForce = 1600;
+    [Range(0.5f, 4f)]
+    public float JumpCooldownInSeconds = 1f;
 
     [Header("Animation")]
     public float AnimatorWalkDampTime = 0.1f;
@@ -16,18 +20,39 @@ public class PlayerMoveController : MonoBehaviour
     public KeyCode SprintKey = KeyCode.LeftShift;
     public KeyCode JumpKey = KeyCode.Space;
 
+    [Header("Is Grounded Check")]
+    public Transform IsGroundedSource;
+    public LayerMask GroundLayer;
+    [Range(0.1f, 1f)]
+    public float IsGroundedRayLenght = 0.2f;
+    [Range(0.2f, 2f)]
+    public float IsFallingRayLenght = 0.8f;
+
     [Header("States")]
     public bool IsMoving;
+    public bool IsSprinting;
+    public bool IsCrouching;
+    public bool IsGrounded;
+    public bool IsJumping;
+    public bool IsFalling;
 
     private Animator _animator;
+    private Rigidbody _rigidBody;
+    private bool _canJump = true;
+    private bool _lastFrameIsFalling;
+    private bool _lastFrameIsGrounded;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
+        SetLastFrameValues();
+        CheckIsGrounded();
+        CheckIsFalling();
         Move();
         Crouch();
         Sprint();
@@ -58,12 +83,14 @@ public class PlayerMoveController : MonoBehaviour
     {
         if (Input.GetKeyDown(CrouchKey))
         {
-            _animator.SetBool("Crouching", true);
+            IsCrouching = true;
+            _animator.SetBool("Crouching", IsCrouching);
         }
 
         if (Input.GetKeyUp(CrouchKey))
         {
-            _animator.SetBool("Crouching", false);
+            IsCrouching = false;
+            _animator.SetBool("Crouching", IsCrouching);
         }
     }
 
@@ -71,20 +98,96 @@ public class PlayerMoveController : MonoBehaviour
     {
         if (Input.GetKeyDown(SprintKey))
         {
+            IsSprinting = true;
             _animator.speed = SprintMultiplier;
         }
 
         if (Input.GetKeyUp(SprintKey))
         {
+            IsSprinting = false;
             _animator.speed = 1f;
         }
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(JumpKey))
+        if (_canJump && Input.GetKeyDown(JumpKey))
         {
-            _animator.SetTrigger("Jump");
+            if (IsGrounded)
+            {
+                IsJumping = true;
+                StartCoroutine(JumpCooldown());
+                var jumpDirection = Vector3.up;
+                if (Mathf.Abs(Input.GetAxis("Vertical") - 0) > 0.2f)
+                {
+                    jumpDirection = (Vector3.up + (transform.forward * (_rigidBody.velocity.magnitude / 4))).normalized;
+                }
+                
+                _animator.applyRootMotion = false;
+                _animator.SetBool("IsJumping", IsJumping);
+                _rigidBody.AddForce(jumpDirection * JumpForce, ForceMode.Force);
+                _canJump = false;
+            }
         }
+    }
+
+    private void CheckIsFalling()
+    {
+        Debug.DrawRay(IsGroundedSource.position, -Vector3.up * IsFallingRayLenght, Color.green);
+        if (Physics.Raycast(IsGroundedSource.position, -Vector3.up, IsFallingRayLenght, GroundLayer))
+        {
+            // Not Falling
+
+            if (_lastFrameIsFalling)
+            {
+                _animator.applyRootMotion = true;
+            }
+
+            IsFalling = false;
+        }
+        else
+        {
+            // Falling
+
+            IsFalling = true;
+        }
+
+        _animator.SetBool("IsFalling", IsFalling);
+    }
+
+    private void CheckIsGrounded()
+    {
+        Debug.DrawRay(IsGroundedSource.position, -Vector3.up * IsGroundedRayLenght, Color.red);
+        if (Physics.Raycast(IsGroundedSource.position, -Vector3.up, IsGroundedRayLenght, GroundLayer))
+        {
+            // Grounded
+
+            IsGrounded = true;
+        }
+        else
+        {
+            // Not Grounded
+
+            if (IsJumping)
+            {
+                IsJumping = false;
+            }
+
+            IsGrounded = false;
+        }
+        _animator.SetBool("IsGrounded", IsGrounded);
+        _animator.SetBool("IsJumping", IsJumping);
+    }
+
+    private void SetLastFrameValues()
+    {
+        _lastFrameIsFalling = IsFalling;
+        _lastFrameIsGrounded = IsGrounded;
+    }
+
+    private IEnumerator JumpCooldown()
+    {
+        yield return new WaitForSeconds(JumpCooldownInSeconds);
+        _canJump = true;
     }
 }
